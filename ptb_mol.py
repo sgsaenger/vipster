df689d688ebeb30e698926248734dc2c66d82af4
hein09 <sebastian_gsaenger@web.de>
2014-06-29 23:20:01 +0200

    Added pwi file handler

--- ptb_mol.py
+++ ptb_mol.py
@@ -5,6 +5,45 @@ import sys
 import copy
 
 ######################################################################
+# PSE DICTIONARY ? suitable for lammps ?
+######################################################################
+# pse[0] id
+# pse[1] ~ weight
+pse={"X":  [0, 0.0],
+
+     "H" : [1, 1.0079],
+     "He": [2,4.0026],
+
+     "Li": [3, 6.941],
+     "Be": [4, 9.0122],
+     "B" : [5,10.811],
+     "C" : [6,12.0107],
+     "N" : [7,14.007],
+     "O" : [8,15.999],
+     "F" : [9,18.998],
+     "Ne": [10,20.18],
+
+     "Na": [11,22.99],
+     "Mg": [12,24.305],
+     "Al": [13,26.982],
+     "Si": [14,28.086],
+     "P" : [15,30.974],
+     "S" : [16,32.065],
+     "Cl": [17,35.453],
+     "Ar": [18,39.948],
+
+     "K" : [19,39.098],
+     "Ca": [20,40.078],
+     "Ga": [31,69.723],
+     "Ge": [32,72.64],
+     "As": [33,74.922],
+     "Se": [34,78.96],
+     "Br": [35,79.904],
+     "Kr": [36,83.798]
+
+     }
+
+######################################################################
 # MAIN CONTROLLER CLASS
 ######################################################################
 class TBController:
@@ -15,8 +54,8 @@ class TBController:
                 #self.data = []
 
         def readFile(self,fmt,filename):
-                parser = {'PWi' : self.readPwi,
-                          'PWo' : self.readPwo,
+                parser = {'PWi' : self.parsePwi,
+                          'PWo' : self.parsePwo,
                           'xyz' : self.parseXyz,
                          }
                 data = open(filename,'r').readlines()
@@ -24,22 +63,27 @@ class TBController:
                 #return data
 
         def parseXyz(self,data):
+                # create list of mol, trajectory support
                 tlist = []
-                molcnt = 0
+                # if first line does not contain nat exit
                 if not data[0].strip().isdigit():
                         print 'not a xyz file'
                         return
                 while data:
-                        molcnt += 1
+                        # handle empty lines at eof or between molecules
                         if not data[0].strip().isdigit():
                                 del data[0]
                                 if not data: break
+                        # create new molecule
                         tmol = Molecule()
+                        # fixed format nat and comment
                         nat = int(data[0].strip())
                         tmol.comment = data[1].strip()
+                        # read coordinates and types
                         for i in range(2,nat+2):
                                 line = data[i].split()
                                 tmol.create_atom(line[0],float(line[1]),float(line[2]),float(line[3]))
+                        # remove parsed lines to check if there's more to do
                         for i in range(0,nat+2):
                                 del data[0]
                         tlist.append(copy.deepcopy(tmol))
@@ -47,10 +91,62 @@ class TBController:
                 #return tlist
                 self.mol = self.mol + tlist
 
-        def readPwi(self):
-                bargl = 0
+        def parsePwi(self,data):
+                # no need for list, only one molecule per file
+                tmol = Molecule()
+                tparam = PWParam()
+                k = 0
+                while data:
+                        k +=1
+                        # ignore empty lines
+                        if not data[0].strip():
+                                del data[0]
+                                print 'empty line'
+                                print data
+                        # parse namelists
+                        elif data[0].strip()[0] == '&':
+                                tnl = tparam.Namelist()
+                                i = 1
+                                # parse entries
+                                while data[i].strip() != '/':
+                                        line = data[i].strip().split(',')
+                                        for j in range(len(line)):
+                                                tnl[line[j].split('=')[0]]=line[j].split('=')[1]
+                                        i +=1
+                                tparam[data[0].strip()]=tnl
+                                # delete parsed namelist
+                                for j in range(i+1):
+                                        del data[0]
+                                print 'new namelist'
+                                print data
+                        # parse card
+                        elif data[0].strip().split()[0].isupper():
+                                i = 1
+                                # 7 types of cards, need hardcoding
+                                # 4 supported for now
+                                # still need modes for all of them
+                                while data[i].strip():
+                                        line = data[i].strip().split()
+                                        if data[0].strip().split()[0] == 'ATOMIC_SPECIES':
+                                                tparam.pse[line[0]][1] = float(line[1])
+                                                tparam.pse[line[0]].append(line[2])
+                                        elif data[0].strip().split()[0] == 'ATOMIC_POSITIONS':
+                                                tmol.create_atom(line[0],float(line[1]),float(line[2]),float(line[3]))
+                                        elif data[0].strip().split()[0] == 'K_POINTS':
+                                                tparam['K_POINTS']=[line[0:3],line[3:7]]
+                                        elif data[0].strip().split()[0] == 'CELL_PARAMETERS':
+                                                tmol.vec[i-1]=[float(line[0]),float(line[1]),float(line[2])]
+                                        i +=1
+                                # delete parsed card
+                                for j in range(i):
+                                        del data[0]
+                                print 'new card'
+                                print data
+                print k
+                self.mol.append(tmol)
+                self.pwdata.append(tparam)
 
-        def readPwo(self):
+        def parsePwo(self,data):
                 bargl = 0
 
         def writeXyz(self,mol,filename=""):
@@ -58,8 +154,10 @@ class TBController:
                         f=sys.stdout
                 else:
                         f=open(filename,'w')
+                # fixed format nat and comment
                 f.write(str(mol.get_nat())+'\n')
                 f.write(mol.get_comment()+'\n')
+                # write coordinates
                 for cntat in range(0,mol.get_nat()):
                         f.write(
                                 '{:4s} {:15.10f} {:15.10f} {:15.10f}'.format(
@@ -106,13 +204,6 @@ class Molecule:
         def del_atom(self,index):
                 del self.at[index]
 
-        # set periodicity
-        def set_periodicity(self,vec0,vec1,vec2,off=[0.0,0.0,0.0]):
-                self.vec[0]=vec0
-                self.vec[1]=vec1
-                self.vec[2]=vec2
-                self.offset=off
-
         ######################################################
         # return functions
         ######################################################
@@ -135,6 +226,20 @@ class Molecule:
                     types.add(i.get_name())
                 return len(types)
 
+        ######################################################
+        # set functions
+        ######################################################
+
+        # set celldm
+        def set_celldm(self,cdm):
+                self.celldm = cdm
+
+        # set periodicity
+        def set_periodicity(self,vec0,vec1,vec2,off=[0.0,0.0,0.0]):
+                self.vec[0]=vec0
+                self.vec[1]=vec1
+                self.vec[2]=vec2
+                self.offset=off
 
 ######################################################################
 # ATOM CLASS
@@ -175,48 +280,21 @@ class Molecule:
         #Kommt spaeter:
         #class Bond:
 
-class PWParam:
+class PWParam(dict):
 
         def __init__(self):
-                self.calctype = 'scf'
-                self.ntyp = 0
+                # make local copy of pse
+                self.pse = copy.copy(pse)
 
+                # k-point grid
+                # only MP-grids (automatic) for now
+                self['K_POINTS']=[[1,1,1],[0,0,0]]
 
-######################################################################
-# PSE DICTIONARY ? suitable for lammps ?
-######################################################################
-# pse[0] id
-# pse[1] ~ weight
-pse={"X":  [0, 0.0],
+        # define NL and Card classes just to distinguish
+        class Namelist(dict):
+                def is_namelist(self):
+                        return True
 
-     "H" : [1, 1.0079],
-     "He": [2,4.0026],
-
-     "Li": [3, 6.941],
-     "Be": [4, 9.0122],
-     "B" : [5,10.811],
-     "C" : [6,12.0107],
-     "N" : [7,14.007],
-     "O" : [8,15.999],
-     "F" : [9,18.998],
-     "Ne": [10,20.18],
-
-     "Na": [11,22.99],
-     "Mg": [12,24.305],
-     "Al": [13,26.982],
-     "Si": [14,28.086],
-     "P" : [15,30.974],
-     "S" : [16,32.065],
-     "Cl": [17,35.453],
-     "Ar": [18,39.948],
-
-     "K" : [19,39.098],
-     "Ca": [20,40.078],
-     "Ga": [31,69.723],
-     "Ge": [32,72.64],
-     "As": [33,74.922],
-     "Se": [34,78.96],
-     "Br": [35,79.904],
-     "Kr": [36,83.798]
-
-     }
+        #class Card(dict):
+        #        def is_namelist(self):
+        #                return False
