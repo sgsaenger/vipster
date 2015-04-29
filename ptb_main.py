56b4df7f4ddae7896aa7f2329842160212094e1d
hein09 <sebastian_gsaenger@web.de>
2015-04-29 14:48:44 +0200

    Moved print_help to ptb_main
    docstrings for ptb_mol

--- ptb_main.py
+++ ptb_main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-from sys import exit,argv
+from sys import exit,argv,stdout
 import cProfile
 from ptb_mol import TBController
 from ptb_gui import MakeWindow
@@ -19,7 +19,8 @@ def main():
     if len(argv)==1:
         backend.newMol()
     elif argv[1] == '-h':
-        backend.print_help(0)
+        #backend.print_help(0)
+        print_help(backend,0)
     elif argv[1] in backend.cli_indict:
         for i in argv[2:]:
             backend.readFile(argv[1],i,'cli')
@@ -31,6 +32,17 @@ def main():
     app.aboutToQuit.connect(gui.deleteLater)
     gui.loadView()
     exit(app.exec_())
+
+def print_help(backend,err):
+    f = stdout
+    f.write('PWToolBox usage:\n')
+    f.write('ptb_main [OPTIONS]\n\n')
+    f.write('No option given: start GUI\n\n')
+    f.write('Options:\n')
+    f.write('-h: print this help\n')
+    for i in backend.cli_indict.items():
+        f.write(i[0]+' [FILES]: '+i[1].__doc__.split('\n')[0]+'\n')
+    exit(err)
 
 if __name__ == '__main__':
         main()
