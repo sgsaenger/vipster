fe16a072900de667435ab3ae52a28b88ef8f6531
hein09 <sebastian_gsaenger@web.de>
2015-06-18 17:22:15 +0200

    modified plane-shift
    replaced target-distance with
    shiftvector relative to plane

--- molecule.py
+++ molecule.py
@@ -467,10 +467,6 @@ class Molecule:
     def set_vol_gradient(self):
         """Calculate the gradient of volume data"""
         self._vol_grad = make_vol_gradient(self._vol)
-        #grad_x = np.roll(self._vol,-1,0) - np.roll(self._vol,1,0)
-        #grad_y = np.roll(self._vol,-1,1) - np.roll(self._vol,1,1)
-        #grad_z = np.roll(self._vol,-1,2) - np.roll(self._vol,1,2)
-        #self._vol_grad = zip(grad_x,grad_y,grad_z)
 
     def get_vol_gradient(self):
         """Return volume gradient"""
@@ -500,7 +496,7 @@ class Molecule:
                 'def':self._evalArgs('define','ls'),
                 'mir':self._evalArgs(self._mirror,'lvvo'),
                 'rep':self._evalArgs('repeat','i'),
-                'psh':self._evalArgs(self._pshift,'lill'),
+                'psh':self._evalArgs(self._pshift,'lvll'),
                 'par':self._evalArgs(self._parallelize,'lll')}
         stack=[]
         #check for errors, parse and prepare
@@ -706,16 +702,22 @@ class Molecule:
         n2=np.cross(d21,d22)
         n2=n2/np.linalg.norm(n2)
         angle=np.degrees(np.arccos(np.dot(n2,n1)))
+        if np.isclose(angle,0,atol=1.e-6) \
+                or np.isclose(angle,0,atol=1.e-6):
+            return
         axis=np.cross(n1,n2)
         self._rotate(atoms,angle,axis,p1[1])
 
-    def _pshift(self,atoms,dist,p1,p2):
+    def _pshift(self,atoms,vec,p1,p2):
         """Shift planes relatively
 
         atoms -> list of atoms
-        dist -> target distance between planes
+        vec -> shift vector
+              x/y parallel to static plane
+              y perpendicular
         p1 -> plane in molecule/mobile target
         p2 -> plane on surface/static target
+              p2[0]-p2[1] give x-coordinate
         """
         if len(p1)!=3:
             raise ValueError('Mobile plane not valid')
@@ -732,9 +734,8 @@ class Molecule:
         n2=np.cross(d21,d22)
         n2=n2/np.linalg.norm(n2)
         angle=np.degrees(np.arccos(np.dot(n2,n1)))
-        if not np.isclose(angle,0,atol=1.e-6):
+        if not np.isclose(angle,0,atol=1.e-6)\
+                and not np.isclose(angle,180,atol=1.e-6):
             raise ValueError('Planes are not parallel')
-        dist1=np.dot(p1[0],n1)
-        dist2=np.dot(p2[0],n2)
-        dist=dist-dist1+dist2
-        self._shift(atoms,dist*n1)
+        shift=np.dot(vec,[d11,np.cross(d11,n2),n2])
+        self._shift(atoms,shift)
