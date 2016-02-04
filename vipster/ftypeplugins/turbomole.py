9d7d643af005a204dc1851d370747775f729d7c8
hein09 <sebastian_gsaenger@web.de>
2016-02-04 14:03:03 +0100

    make turbomole parsing less strict
    bugfix: return (Mol,Param)-tuple

--- vipster/ftypeplugins/turbomole.py
+++ vipster/ftypeplugins/turbomole.py
@@ -2,20 +2,21 @@
 from ..molecule import Molecule
 
 name = 'turbomole'
-extension = ''
-argument = 'turbomole'
+extension = 'turbo'
+argument = 'turbo'
 
 param = None
 
 def parser(name,data):
     """ Parse turbomole specific input file """
     tmol = Molecule(name)
-    tmol.setVec([[1,0,0],[0,1,0],[0,0,1]])
-    tmol.setCellDim(40.0)
-    for l in data[1:]:
-        if '$' in l: break
-        line = l.split()
-        tmol.newAtom(line[3].upper(),line[0:3],'bohr')
-    return tmol
+    for i in range(len(data)):
+        if "$coord" in data[i]:
+            for l in data[i+1:]:
+                if '$' in l: break
+                line = l.split()
+                tmol.newAtom(line[3].capitalize(),line[0:3],'bohr')
+            break
+    return tmol,None
 
 writer = None
