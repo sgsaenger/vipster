aecfc6dbd78c799aefca7bcb4d5b667cd77870bf
hein09 <sebastian_gsaenger@web.de>
2017-01-09 22:28:23 +0100

    cleaned up namespace-stuff, added checks for nat/ntyp

--- libvipster/ioplugins/pwinput.cpp
+++ libvipster/ioplugins/pwinput.cpp
@@ -1,5 +1,6 @@
 #include "ioplugins/pwinput.h"
 #define BOOST_SPIRIT_DEBUG
+#define BOOST_SPIRIT_USE_PHOENIX_V3
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/support_multi_pass.hpp>
 #include <boost/spirit/include/phoenix.hpp>
@@ -104,7 +105,7 @@ BOOST_FUSION_ADAPT_STRUCT(
 
 template<typename Iterator>
 struct pwi_parse_grammar
-        : qi::grammar<Iterator, Vipster::IO::PWData(), qi::blank_type>
+        : qi::grammar<Iterator, IO::PWData(), qi::locals<int,int,int>, qi::blank_type>
 {
     pwi_parse_grammar(): pwi_parse_grammar::base_type(file)
     {
@@ -135,19 +136,21 @@ struct pwi_parse_grammar
         psepair = name > pseentry;
         psepair.name("PSE KV-pair");
         species = qi::no_case[qi::lit("atomic_species")] > qi::eol
-                > (psepair % qi::eol);
+                > (psepair % qi::eol)
+                > qi::eps(qi::_r1 == phx::bind(&vector<pair<string,PseEntry>>::size,qi::_val));
         species.name("Atomic species");
         atom = name > qi::double_ > qi::double_ > qi::double_;
         atom.name("Atom");
-        format = qi::no_case[qi::lit("crystal")][phx::ref(fmt) = Vipster::AtomFmt::Crystal]
-               | qi::no_case[qi::lit("alat")][phx::ref(fmt) = Vipster::AtomFmt::Alat]
-               | qi::no_case[qi::lit("bohr")][phx::ref(fmt) = Vipster::AtomFmt::Bohr]
-               | qi::no_case[qi::lit("angstrom")][phx::ref(fmt) = Vipster::AtomFmt::Angstrom];
+        format = qi::no_case[qi::lit("crystal")][phx::ref(fmt) = AtomFmt::Crystal]
+               | qi::no_case[qi::lit("alat")][phx::ref(fmt) = AtomFmt::Alat]
+               | qi::no_case[qi::lit("bohr")][phx::ref(fmt) = AtomFmt::Bohr]
+               | qi::no_case[qi::lit("angstrom")][phx::ref(fmt) = AtomFmt::Angstrom];
         format.name("Atom format");
         atoms = qi::no_case[qi::lit("atomic_positions")] > -format > qi::eol
-                > (atom % qi::eol);
+                > (atom % qi::eol)
+                > qi::eps(qi::_r1 == phx::bind(&vector<Atom>::size,qi::_val));
         atoms.name("Atomic positions");
-        positions = qi::as<Vipster::Step>()[atoms];
+        positions = qi::as<Step>()[atoms(qi::_r1)];
         kpointmpg = qi::eol > qi::int_ > qi::int_ > qi::int_ > qi::double_ > qi::double_ > qi::double_;
         disckpoint = qi::double_ > qi::double_ > qi::double_ > qi::double_;
         disckpoints = (disckpoint % qi::eol);
@@ -158,17 +161,21 @@ struct pwi_parse_grammar
                      ) > qi::eol > qi::omit[qi::int_] > qi::eol > (disckpoint % qi::eol);
         kpoints = qi::no_case[qi::lit("k_points")] >
                    (qi::no_case[qi::lit("gamma")]
-                   |(qi::no_case[qi::lit("automatic")] > qi::attr(Vipster::KPointFmt::MPG) > kpointmpg)
-                   |(qi::attr(Vipster::KPointFmt::Discrete) > qi::attr(Vipster::KPoints::MPG{}) > kpointdisc));
+                   |(qi::no_case[qi::lit("automatic")] > qi::attr(KPointFmt::MPG) > kpointmpg)
+                   |(qi::attr(KPointFmt::Discrete) > qi::attr(KPoints::MPG{}) > kpointdisc));
         mol.name("Molecule");
-        mol = qi::omit[species] > *qi::eol >
-              positions > *qi::eol >
+        mol = qi::omit[species(qi::_r1)] > *qi::eol >
+              positions(qi::_r2) > *qi::eol >
               kpoints > *qi::eol;// >
-//              cell > *qi::eol >
+//              cell(qi::_r3) > *qi::eol >
 //              occupations > *qi::eol >
 //              constraints > *qi::eol >
 //              forces > *qi::eol;
-        file = param > *qi::eol > mol;
+        file %= param >
+                qi::eps[qi::_a = phx::bind([](const IO::PWData& p){return stoi(p.data.system.at("ntyp"));}, qi::_val)] >
+                qi::eps[qi::_b = phx::bind([](const IO::PWData& p){return stoi(p.data.system.at("nat"));}, qi::_val)] >
+                qi::eps[qi::_c = phx::bind([](const IO::PWData& p){return stoi(p.data.system.at("ibrav"));}, qi::_val)] >
+                *qi::eol > mol(qi::_a,qi::_b,qi::_c);
         file.name("PWScf input file");
         qi::on_error<qi::fail>(
                 file,
@@ -188,40 +195,39 @@ struct pwi_parse_grammar
 //        BOOST_SPIRIT_DEBUG_NODE(key);
 //        BOOST_SPIRIT_DEBUG_NODE(fstr);
 //        BOOST_SPIRIT_DEBUG_NODE(mol);
-        BOOST_SPIRIT_DEBUG_NODE(name);
+//        BOOST_SPIRIT_DEBUG_NODE(name);
 //        BOOST_SPIRIT_DEBUG_NODE(species);
 //        BOOST_SPIRIT_DEBUG_NODE(psepair);
 //        BOOST_SPIRIT_DEBUG_NODE(pseentry);
-        BOOST_SPIRIT_DEBUG_NODE(atoms);
-        BOOST_SPIRIT_DEBUG_NODE(atom);
-        BOOST_SPIRIT_DEBUG_NODE(format);
+//        BOOST_SPIRIT_DEBUG_NODE(atoms);
+//        BOOST_SPIRIT_DEBUG_NODE(atom);
+//        BOOST_SPIRIT_DEBUG_NODE(format);
     }
-    Vipster::AtomFmt fmt = Vipster::AtomFmt::Alat;
-    qi::rule<Iterator, Vipster::IO::PWData(), qi::blank_type> file;
+    AtomFmt fmt = AtomFmt::Alat;
+    Mat m={{{1,0,0},{0,1,0},{0,0,1}}};
+    qi::rule<Iterator, IO::PWData(), qi::locals<int,int,int>, qi::blank_type> file;
 //Parameter-set, composed of F90-namelists
-    qi::rule<Iterator, Vipster::IO::PWParam(), qi::blank_type> param;
+    qi::rule<Iterator, IO::PWParam(), qi::blank_type> param;
     qi::rule<Iterator, map<string, string>(string), qi::blank_type> namelist;
     qi::rule<Iterator, pair<string, string>(), qi::blank_type> nl_entry;
     qi::rule<Iterator, string()> value;
     qi::rule<Iterator, string()> key;
     qi::rule<Iterator, string()> fstr;
 //Molecule
-    qi::rule<Iterator, Vipster::Molecule(), qi::blank_type> mol;
-    qi::rule<Iterator, vector<Vipster::Step>(), qi::blank_type> positions;
-    qi::rule<Iterator, Vipster::Step(), qi::blank_type> step;
+    qi::rule<Iterator, Molecule(int,int,int), qi::blank_type> mol;
     qi::rule<Iterator, string()> name;
-    qi::rule<Iterator, vector<pair<string,Vipster::PseEntry>>(), qi::blank_type> species;
-    qi::rule<Iterator, pair<string,Vipster::PseEntry>(), qi::blank_type> psepair;
-    qi::rule<Iterator, Vipster::PseEntry(), qi::blank_type> pseentry;
-    qi::rule<Iterator, vector<Vipster::Atom>(), qi::blank_type> atoms;
+    qi::rule<Iterator, vector<pair<string,PseEntry>>(int), qi::blank_type> species;
+    qi::rule<Iterator, pair<string,PseEntry>(), qi::blank_type> psepair;
+    qi::rule<Iterator, PseEntry(), qi::blank_type> pseentry;
+    qi::rule<Iterator, vector<Step>(int), qi::blank_type> positions;
+    qi::rule<Iterator, vector<Atom>(int), qi::blank_type> atoms;
     qi::rule<Iterator, qi::blank_type> format;
-    qi::rule<Iterator, Vipster::Atom(), qi::blank_type> atom;
-    qi::rule<Iterator, Vipster::KPoints(), qi::blank_type> kpoints;
-    qi::rule<Iterator, Vipster::KPoints::MPG(), qi::blank_type> kpointmpg;
-    qi::rule<Iterator, Vipster::KPoints::Discrete(), qi::blank_type> kpointdisc;
-    qi::rule<Iterator, vector<Vipster::DiscreteKPoint>(), qi::blank_type> disckpoints;
-    qi::rule<Iterator, Vipster::DiscreteKPoint(), qi::blank_type> disckpoint;
-//    qi::rule<Iterator, qi::unused_type(), qi::blank_type> cell;
+    qi::rule<Iterator, Atom(), qi::blank_type> atom;
+    qi::rule<Iterator, KPoints(), qi::blank_type> kpoints;
+    qi::rule<Iterator, KPoints::MPG(), qi::blank_type> kpointmpg;
+    qi::rule<Iterator, KPoints::Discrete(), qi::blank_type> kpointdisc;
+    qi::rule<Iterator, DiscreteKPoint(), qi::blank_type> disckpoint;
+//    qi::rule<Iterator, qi::unused_type(int), qi::blank_type> cell;
 //    qi::rule<Iterator, qi::unused_type(), qi::blank_type> occupations;
 //    qi::rule<Iterator, qi::unused_type(), qi::blank_type> constraints;
 //    qi::rule<Iterator, qi::unused_type(), qi::blank_type> forces;
