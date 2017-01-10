e511fba435d6683aae817fee6436da1bebc97f06
Sebastian Gsänger <sebastian_gsaenger@web.de>
2017-01-10 17:37:01 +0100

    working qmake build, Vec and Atom

--- python/python.pro
+++ python/python.pro
@@ -10,13 +10,24 @@ TARGET = vipster
 TEMPLATE = lib
 
 DEFINES += LIBVIPSTER_PYTHON
+CONFIG += c++14 plugin no_plugin_name_prefix
 
 SOURCES += \
     vipster.cpp
 
 HEADERS +=
 
+QMAKE_CFLAGS += $$system(python-config --cflags)
+QMAKE_CXXFLAGS += $$system(python-config --cflags)
+
 unix {
     target.path = /usr/lib
     INSTALLS += target
 }
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../libvipster/release/ -lvipster
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../libvipster/debug/ -lvipster
+else:unix: LIBS += -L$$OUT_PWD/../libvipster/ -lvipster
+
+INCLUDEPATH += $$PWD/../libvipster
+DEPENDPATH += $$PWD/../libvipster
--- python/vipster.cpp
+++ python/vipster.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
-#include <vipster/molecule.h>
+#include <pybind11/operators.h>
+#include <molecule.h>
 
 int add(int i, int j) {
     return (i + 3) * j;
@@ -7,15 +8,37 @@ int add(int i, int j) {
 
 namespace py = pybind11;
 using namespace py::literals;
+using namespace Vipster;
 
-PYBIND11_PLUGIN(test) {
-    py::module m("test", "pybind11 example plugin");
+PYBIND11_PLUGIN(vipster) {
+    py::module m("vipster", "pybind11 example plugin");
 
     m.def("add", &add, "A function which adds two numbers",
             "i"_a=1, "j"_a=2);
     m.attr("what") = py::cast("Bargl?!");
 
-    py::class_<Vipster::Atom>(m, "Atom");
+    py::class_<Vec>(m, "Vec")
+        .def(py::init())
+        .def_property("x",[](const Vec &v){return v[0];},[](Vec &v, float f){v[0] = f;})
+        .def_property("y",[](const Vec &v){return v[1];},[](Vec &v, float f){v[1] = f;})
+        .def_property("z",[](const Vec &v){return v[2];},[](Vec &v, float f){v[2] = f;})
+        .def("__bool__",[](const Vec&){return true;})
+        //.dev("__repr__",[](....TODO
+        .def(py::self == py::self);
+//    py::detail::vector_if_copy_constructible; DONE
+//    py::detail::vector_if_equal_operator;DONE
+//    py::detail::vector_if_insertion_operator;DONE
+//    py::detail::vector_modifiers; TODO setitem/getitem
+//    py::detail::vector_accessor; TODO getitem/iter
+
+    py::class_<Atom>(m, "Atom")
+        .def(py::init())
+        .def_readwrite("name",&Atom::name)
+        .def_readwrite("coord",&Atom::coord)
+        .def_readwrite("charge",&Atom::charge)
+        .def_readwrite("fix",&Atom::fix)
+        .def_readwrite("hidden",&Atom::hidden)
+        .def(py::self == py::self);
 
     return m.ptr();
 }
