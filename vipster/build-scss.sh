411ab32063697605fc2b420438d9c093a48e46e1
Jonathan Gruber <gruberjonathan@gmail.com>
2018-02-06 03:25:27 +0000

    Added Post CSS Autoprefixer

--- vipster/build-scss.sh
+++ vipster/build-scss.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+
 SCRIPT=`realpath $0`
 PWD=`dirname $SCRIPT`
 
-sassc -t compressed --sourcemap $PWD/styles/styles.scss $PWD/styles/styles.css
+sassc -t compressed $PWD/styles/styles.scss $PWD/styles/styles.css
+npx postcss $PWD/styles/styles.css --use autoprefixer -r
