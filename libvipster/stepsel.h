e4bae0a6af35f41c3f46a109f19b57609ba88404
Sebastian Gsänger <sebastian_gsaenger@web.de>
2018-05-28 16:44:28 +0200

    added filtering with negation and by type, position and index

--- libvipster/stepsel.h
+++ libvipster/stepsel.h
@@ -4,13 +4,24 @@
 #include "step.h"
 
 namespace Vipster {
+
+/*
+ * Selection container
+ *
+ * contains indices of selected atoms in AtomList
+ */
+struct AtomSelection{
+    std::vector<size_t> indices;
+    std::shared_ptr<AtomList> atoms;
+};
+
 /*
  * Wrap multiple filter criteria without polymorphism
  * also, represents differently coupled filter chains
  * Target grammar:
  * Keywords are case insensitive, whereas atom-types are case-sensitive
  *
- * Filter ::= Criterion, [("(", Coupling, Filter, ")")];
+ * Filter ::= Criterion, [(Coupling, Filter)];
  * Criterion ::= ["not "], (TypeCrit | IdxCrit | PosCrit | CoordCrit);
  * Coupling ::= ["!"], ("|" | "&" | "^");
  *
@@ -18,12 +29,13 @@ namespace Vipster {
  * TypeList ::= "[", Type, {(" ", Type)}, "]";
  * Type ::= NonWhiteSpace, {NonWhiteSpace};
  *
- * IdxCrit ::= "index " , IdxList;
- * IdxList ::= ( "[", IdxRange, {(" " IdxRange)}, "]") | IdxRange;
+ * IdxCrit ::= "index " , (IdxList | IdxRange);
+ * IdxList ::= ( "[", IdxRange, {(" " IdxRange)}, "]");
  * IdxRange ::= ( Integer, "-", Integer) | Integer;
  *
- * PosCrit ::= "pos ", Direction, CompOp, Float;
+ * PosCrit ::= "pos ", Direction, Format, CompOp, Float;
  * Direction ::= "x" | "y" | "z";
+ * Format ::= "a" | "b" | "c" | "d";
  * CompOp ::= ">" | "<";
  *
  * CoordCrit ::= "coord ", CompEqOp, Integer;
@@ -37,23 +49,34 @@ struct SelectionFilter{
           types{f.types},
           subfilter{std::make_unique<SelectionFilter>(*f.subfilter)}
     {}
-    enum class Mode{Index, Type, Coord, Pos};
+    enum class Mode:uint8_t{Index, Type, Coord, Pos};
     enum Op{NONE=0x0, NOT=0x1, // first bit negates own op
             PAIR=0x2, NOT_PAIR=0x4, // second bit activates coupling, third bit negates
             AND=0x2, NAND=0x6,
             OR=0xA, NOR=0xE,
             XOR=0x12, XNOR=0x16,
             UPDATE=0x80};
+    enum Pos{BOHR=0x0, ANG=0x1, CRYS=0x2, CDM=0x3, FMT_MASK=0x3,// 2 bits for format
+             P_GT=0x0, P_LT=0x4, P_CMP_MASK=0x4,  // 1 bit for comp direction
+             X=0x0, Y=0x8, Z=0x10, DIR_MASK=0x18, // 2  bits for space direction
+            };
+    enum Coord{C_GT=0x0, C_EQ=0x1, C_LT=0x2, C_CMP_MASK=0x3};
     Mode mode;
-    unsigned int op;
-    std::vector<size_t> indices;
-    std::vector<std::string> types;
+    uint8_t op;
+    uint8_t pos;
+    uint8_t coord;
+    float posVal;
+    size_t coordVal;
+    std::set<size_t> indices;
+    std::set<std::string> types;
     std::unique_ptr<SelectionFilter> subfilter{nullptr};
 };
 
-std::stringstream& operator<<(std::stringstream& os, const SelectionFilter& filter)
+std::ostream& operator<<(std::ostream& os, const SelectionFilter& filter)
 {
     using Op = SelectionFilter::Op;
+    using Pos = SelectionFilter::Pos;
+    using Coord = SelectionFilter::Coord;
     using Mode = SelectionFilter::Mode;
     if(filter.op & Op::NOT){
         os << "not ";
@@ -61,20 +84,72 @@ std::stringstream& operator<<(std::stringstream& os, const SelectionFilter& filt
     switch(filter.mode){
     case Mode::Index:
         os << "index ";
-        // TODO
+        if(filter.indices.size() == 1){
+            os << *filter.indices.cbegin();
+        }else{
+            os << "[ ";
+            for(const auto& i:filter.indices){
+                os << i << " ";
+            }
+            os << ']';
+        }
         break;
     case Mode::Coord:
         os << "coord ";
-        // TODO
+        switch(filter.coord & Coord::C_CMP_MASK){
+        case Coord::C_GT:
+            os << '>';
+            break;
+        case Coord::C_EQ:
+            os << '=';
+            break;
+        case Coord::C_LT:
+            os << '<';
+            break;
+        }
+        os << filter.coordVal;
         break;
     case Mode::Pos:
         os << "pos ";
-        // TODO
+        switch(filter.pos & Pos::DIR_MASK){
+        case Pos::X:
+            os << 'x';
+            break;
+        case Pos::Y:
+            os << 'y';
+            break;
+        case Pos::Z:
+            os << 'z';
+            break;
+        }
+        switch(filter.pos & Pos::FMT_MASK){
+        case Pos::ANG:
+            os << 'a';
+            break;
+        case Pos::BOHR:
+            os << 'b';
+            break;
+        case Pos::CRYS:
+            os << 'c';
+            break;
+        case Pos::CDM:
+            os << 'd';
+            break;
+        }
+        switch(filter.pos & Pos::P_CMP_MASK){
+        case Pos::P_LT:
+            os << '<';
+            break;
+        case Pos::P_GT:
+            os << '>';
+            break;
+        }
+        os << filter.posVal;
         break;
     case Mode::Type:
         os << "type ";
         if(filter.types.size() == 1){
-            os << filter.types.back();
+            os << *filter.types.cbegin();
         }else{
             os << "[ ";
             for(const auto& t:filter.types){
@@ -102,7 +177,7 @@ std::stringstream& operator<<(std::stringstream& os, const SelectionFilter& filt
     return os;
 }
 
-std::stringstream& operator>>(std::stringstream& is, SelectionFilter& filter){
+std::istream& operator>>(std::istream& is, SelectionFilter& filter){
     using Op = SelectionFilter::Op;
     using Mode = SelectionFilter::Mode;
     std::string token;
@@ -119,33 +194,129 @@ std::stringstream& operator>>(std::stringstream& is, SelectionFilter& filter){
         is >> token;
         if(token[0] == '['){
             if(token.size() > 1){
-                filter.types.push_back(token.substr(1));
+                filter.types.insert(token.substr(1));
             }
             while(is.good()){
                 is >> token;
                 if(token.back() == ']'){
-                    token.pop_back();
-                    if(!token.empty()){
-                        filter.types.push_back(token);
+                    if(token.size() > 1){
+                        token.pop_back();
+                        filter.types.insert(token);
                     }
                     break;
                 }else{
-                    filter.types.push_back(token);
+                    filter.types.insert(token);
                 }
             }
         }else{
             is >> token;
-            filter.types.push_back(token);
+            filter.types.insert(token);
         }
     }else if(token == "coord"){
+        using Coord = SelectionFilter::Coord;
         filter.mode = Mode::Coord;
-        // TODO
+        filter.coord = 0;
+        char ctoken;
+        is >> ctoken;
+        switch(ctoken){
+        case '<':
+            filter.coord |= Coord::C_LT;
+            break;
+        case '>':
+            filter.coord |= Coord::C_GT;
+            break;
+        case '=':
+            filter.coord |= Coord::C_EQ;
+            break;
+        default:
+            throw Error(std::string{"Unknown comparison "}+ctoken);
+        }
+        is >> filter.coordVal;
     }else if(token == "pos"){
+        using Pos = SelectionFilter::Pos;
         filter.mode = Mode::Pos;
-        // TODO
+        filter.pos = 0;
+        char ctoken;
+        is >> ctoken;
+        switch(ctoken){
+        case 'x':
+            filter.pos |= Pos::X;
+            break;
+        case 'y':
+            filter.pos |= Pos::Y;
+            break;
+        case 'z':
+            filter.pos |= Pos::Z;
+            break;
+        default:
+            throw Error(std::string{"Unknown direction "}+ctoken);
+        }
+        is >> ctoken;
+        switch(ctoken){
+        case 'a':
+            filter.pos |= Pos::ANG;
+            break;
+        case 'b':
+            filter.pos |= Pos::BOHR;
+            break;
+        case 'c':
+            filter.pos |= Pos::CRYS;
+            break;
+        case 'd':
+            filter.pos |= Pos::CDM;
+            break;
+        default:
+            throw Error(std::string{"Unknown format "}+ctoken);
+        }
+        is >> ctoken;
+        switch(ctoken){
+        case '<':
+            filter.pos |= Pos::P_LT;
+            break;
+        case '>':
+            filter.pos |= Pos::P_GT;
+            break;
+        default:
+            throw Error(std::string{"Unknown comparison "}+ctoken);
+        }
+        is >> filter.posVal;
     }else if(token == "index"){
         filter.mode = Mode::Index;
-        // TODO
+        is >> token;
+        auto rangeParser = [&filter](const std::string &token){
+            auto splitPos = token.find('-');
+            if(splitPos != token.npos){
+                auto left = std::stol(token.substr(0, splitPos));
+                auto right = std::stol(token.substr(splitPos+1));
+                for(auto i=std::min(left, right); i<=std::max(left,right);++i){
+                    filter.indices.insert(static_cast<size_t>(i));
+                }
+            }else{
+                filter.indices.insert(std::stoul(token));
+            }
+        };
+        if(token[0] == '['){
+            if(token.size() > 1){
+                rangeParser(token.substr(1));
+            }
+            while(is.good()){
+                is >> token;
+                if(token.back() == ']'){
+                    if(token.size() > 1){
+                        token.pop_back();
+                        rangeParser(token);
+                    }
+                    break;
+                }else{
+                    rangeParser(token);
+                }
+            }
+        }else{
+            is >> token;
+            rangeParser(token);
+        }
+    }else{
+        throw Error("Unknown selection operator: "+token);
     }
     auto ctoken = is.peek();
     if(ctoken == '('){
@@ -176,24 +347,15 @@ std::stringstream& operator>>(std::stringstream& is, SelectionFilter& filter){
 }
 
 /*
- * Selection container
- *
- * contains indices of selected atoms in AtomList
- */
-struct AtomSelection{
-    std::vector<size_t> indices;
-    std::shared_ptr<AtomList> atoms;
-};
-
-/*
  * recursively evaluate the filters
  */
-std::vector<size_t> evalFilter(const AtomList& atoms, SelectionFilter& filter)
+std::vector<size_t> evalFilter(const Step& step, const SelectionFilter& filter)
 {
     std::vector<size_t> tmp;
-    if(filter.mode == SelectionFilter::Mode::Type){
-        size_t idx{0};
-        for(const auto& at: atoms.names){
+    size_t idx{0};
+    switch(filter.mode){
+    case SelectionFilter::Mode::Type:
+        for(const auto& at: step.getNames()){
             for(const auto& type: filter.types){
                 if(at == type){
                     tmp.push_back(idx);
@@ -202,6 +364,63 @@ std::vector<size_t> evalFilter(const AtomList& atoms, SelectionFilter& filter)
             }
             ++idx;
         }
+        break;
+    case SelectionFilter::Mode::Index:
+        tmp = std::vector<size_t>{
+                filter.indices.cbegin(), filter.indices.cend()};
+        break;
+    case SelectionFilter::Mode::Pos:
+        {
+        auto cmp = [&filter](const Vec& at){
+            size_t dir = (filter.pos & filter.DIR_MASK) >> 3;
+            if(filter.pos & filter.P_CMP_MASK){
+                return at[dir] < filter.posVal;
+            }
+            return at[dir] > filter.posVal;
+        };
+        for(const auto& at: step.asFmt(static_cast<AtomFmt>(filter.pos & filter.FMT_MASK))
+                                .getCoords()){
+            if(cmp(at)){
+                tmp.push_back(idx);
+            }
+            ++idx;
+        }
+        }
+        break;
+    case SelectionFilter::Mode::Coord:
+        {
+        // TODO: move functionality to step?
+        std::map<size_t, size_t> coord_numbers;
+        for(const Bond& b: step.getBonds()){
+            coord_numbers[b.at1] += 1;
+            coord_numbers[b.at2] += 1;
+        }
+        auto cmp = [&filter](const size_t c){
+            auto cmp_op = filter.coord & filter.C_CMP_MASK;
+            if(cmp_op == filter.C_GT){
+                return c > filter.coordVal;
+            }else if(cmp_op == filter.C_EQ){
+                return c == filter.coordVal;
+            }else{
+                return c < filter.coordVal;
+            }
+        };
+        for(size_t i=0; i<step.getNat(); ++i){
+            if(cmp(coord_numbers[i])){
+                tmp.push_back(i);
+            }
+        }
+        }
+        break;
+    }
+    if(filter.op & filter.NOT){
+        std::vector<size_t> tmp2;
+        for(size_t i=0; i<step.getNat(); ++i){
+            if(std::find(tmp.begin(), tmp.end(), i) == tmp.end()){
+                tmp2.push_back(i);
+            }
+        }
+        std::swap(tmp, tmp2);
     }
     return tmp;
 }
@@ -295,32 +514,45 @@ public:
         this->step = s.step;
         return *this;
     }
-    //TODO: missing constructors?
+    // TODO: missing constructors?
     // TODO: direct Prop-getters?
 
     void        evaluateCache() const override
     {
+        using Mode = SelectionFilter::Mode;
         bool updateNeeded{static_cast<bool>(filter->op & SelectionFilter::UPDATE)};
-        if(!updateNeeded){
-            switch(filter->mode){
-            case SelectionFilter::Mode::Type:
-                if(step.atoms->name_changed)
-                    updateNeeded = true;
-                break;
-            case SelectionFilter::Mode::Coord:
-                // TODO
-                break;
-            case SelectionFilter::Mode::Index:
-                // TODO
-                break;
-            case SelectionFilter::Mode::Pos:
-                // TODO
-                break;
+        // Type or Index need to be reset when types have been changed
+        if(!updateNeeded &&
+                ((filter->mode == Mode::Type) ||
+                 (filter->mode == Mode::Index))){
+            if(step.atoms->name_changed)
+                updateNeeded = true;
+        }
+        // Pos or Index need to be reset when coords have been changed
+        if(!updateNeeded &&
+                ((filter->mode == Mode::Pos) ||
+                 (filter->mode == Mode::Index))){
+            if(std::any_of(step.atoms->coord_changed.begin(), // any coordinates changed,
+                           step.atoms->coord_changed.end(),   // no caches evaluated
+                           [](bool b){return b;}) ||
+                step.atoms->coord_outdated[filter->pos &      // something changed,
+                     SelectionFilter::FMT_MASK]){             // relevant cache still outdated
+                updateNeeded = true;
             }
         }
-        step.evaluateCache();
+        // make sure that caches are clean, for pos even the needed formatted cache
+        auto fmt = (filter->mode == Mode::Pos) ?
+                    static_cast<AtomFmt>(filter->pos & SelectionFilter::FMT_MASK) :
+                    this->at_fmt;
+        step.asFmt(fmt).evaluateCache();
+        // Coord needs to be reset when bonds (possibly) change
+        if(!updateNeeded && (filter->mode == Mode::Coord)){
+            if(step.bonds->outdated){
+                updateNeeded = true;
+            }
+        }
         if(updateNeeded){
-            selection->indices = evalFilter(*selection->atoms, *filter);
+            selection->indices = evalFilter(step, *filter);
         }
     }
     std::string getFilter() const noexcept
--- tests/stepsel.cpp
+++ tests/stepsel.cpp
@@ -4,21 +4,84 @@ using namespace Vipster;
 
 TEST_CASE("Vipster::StepSel", "[step]") {
     StepProper s{};
+    s.setFmt(AtomFmt::Bohr);
     s.newAtom("C");
-    s.newAtom("H");
-    s.newAtom("O");
-    auto selC = s.select("type C");
-    CHECK(selC.getNat() == 1);
-    CHECK(selC[0] == s[0]);
-    auto selH = s.select("type H");
-    CHECK(selH.getNat() == 1);
-    CHECK(selH[0] == s[1]);
-    auto selO = s.select("type O");
-    CHECK(selO.getNat() == 1);
-    CHECK(selO[0] == s[2]);
-    auto selCHO = s.select("type [C H O]");
-    CHECK(selCHO.getNat() == 3);
-    CHECK(selCHO[0] == s[0]);
-    CHECK(selCHO[1] == s[1]);
-    CHECK(selCHO[2] == s[2]);
+    s.newAtom("H", Vec{1,0,0});
+    s.newAtom("O", Vec{2,2,2});
+    SECTION("by type"){
+        auto selC = s.select("type C");
+        CHECK(selC.getNat() == 1);
+        CHECK(selC[0] == s[0]);
+        auto selH = s.select("type H");
+        CHECK(selH.getNat() == 1);
+        CHECK(selH[0] == s[1]);
+        auto selO = s.select("type O");
+        CHECK(selO.getNat() == 1);
+        CHECK(selO[0] == s[2]);
+        auto selCHO = s.select("type [C H O]");
+        CHECK(selCHO.getNat() == 3);
+        CHECK(selCHO[0] == s[0]);
+        CHECK(selCHO[1] == s[1]);
+        CHECK(selCHO[2] == s[2]);
+    }
+    SECTION("by index"){
+        auto sel0 = s.select("index 0");
+        CHECK(sel0.getNat() == 1);
+        CHECK(sel0[0] == s[0]);
+        auto sel1 = s.select("index 1");
+        CHECK(sel1.getNat() == 1);
+        CHECK(sel1[0] == s[1]);
+        auto sel2 = s.select("index 2");
+        CHECK(sel2.getNat() == 1);
+        CHECK(sel2[0] == s[2]);
+        auto selRange = s.select("index 0-2");
+        CHECK(selRange.getNat() == 3);
+        CHECK(selRange[0] == s[0]);
+        CHECK(selRange[1] == s[1]);
+        CHECK(selRange[2] == s[2]);
+        auto selList = s.select("index [0 2]");
+        CHECK(selList.getNat() == 2);
+        CHECK(selList[0] == s[0]);
+        CHECK(selList[1] == s[2]);
+        auto selListRange = s.select("index [1-2 0]");
+        CHECK(selListRange.getNat() == 3);
+        CHECK(selListRange[0] == s[0]);
+        CHECK(selListRange[1] == s[1]);
+        CHECK(selListRange[2] == s[2]);
+        auto selOverdefined = s.select("index [0 0-2]");
+        CHECK(selOverdefined.getNat() == 3);
+        CHECK(selOverdefined[0] == s[0]);
+        CHECK(selOverdefined[1] == s[1]);
+        CHECK(selOverdefined[2] == s[2]);
+    }
+    SECTION("by pos"){
+        s.setCellDim(2, CdmFmt::Bohr);
+        s.setCellVec(Mat{Vec{1,1,0}, Vec{0,1,0}, Vec{0,0,1}});
+        auto selA = s.select("pos xa>0");
+        CHECK(selA.getNat() == 2);
+        auto selB = s.select("pos xb>0");
+        CHECK(selB.getNat() == 2);
+        auto selC = s.select("pos y c < 0");
+        CHECK(selC.getNat() == 1);
+        auto selD = s.select("pos xd > 0.5");
+        CHECK(selD.getNat() == 1);
+    }
+    SECTION("by coord"){
+        auto selEQ = s.select("coord =1");
+        CHECK(selEQ.getNat() == 2);
+        auto selGT = s.select("coord >0");
+        CHECK(selGT.getNat() == 2);
+        auto selLT = s.select("coord <1");
+        CHECK(selLT.getNat() == 1);
+    }
+    SECTION("not"){
+        auto notIdxList = s.select("not index [0 2]");
+        CHECK(notIdxList.getNat() == 1);
+        auto notTypeList = s.select("not type [C O]");
+        CHECK(notTypeList.getNat() == 1);
+        auto notCoordLT = s.select("not coord <1");
+        CHECK(notCoordLT.getNat() == 2);
+        auto notPosB = s.select("not pos xb>0");
+        CHECK(notPosB.getNat() == 1);
+    }
 }
