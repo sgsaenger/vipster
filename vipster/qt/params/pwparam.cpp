6d8faf574ae931ccecf44c055f193de00906f5a6
Sebastian Gsänger <sebastian_gsaenger@web.de>
2018-06-25 16:55:44 +0200

    implement pwparam-widget editing

--- vipster/qt/params/pwparam.cpp
+++ vipster/qt/params/pwparam.cpp
@@ -8,15 +8,24 @@ PWParam::PWParam(QWidget *parent) :
     ui(new Ui::PWParam)
 {
     ui->setupUi(this);
+    addAction = new QAction{"Add Element", ui->paramTree};
+    ui->paramTree->addAction(addAction);
+    connect(addAction, &QAction::triggered, this, &PWParam::addElement);
+    delAction = new QAction{"Delete Element", ui->paramTree};
+    ui->paramTree->addAction(delAction);
+    connect(delAction, &QAction::triggered, this, &PWParam::delElement);
 }
 
 PWParam::~PWParam()
 {
     delete ui;
+    delete addAction;
+    delete delAction;
 }
 
 void PWParam::setParam(BaseParam *p)
 {
+    auto treeBlocker = QSignalBlocker{ui->paramTree};
     curParam = static_cast<IO::PWParam*>(p);
     IO::PWNamelist IO::PWParam::* namelists[] = {
         &IO::PWParam::control, &IO::PWParam::system,
@@ -39,7 +48,76 @@ void PWParam::setParam(BaseParam *p)
         }
         // add new entries
         for(auto& entry: nl){
-            treeTop->addChild(new QTreeWidgetItem{{entry.first.c_str(), entry.second.c_str()}});
+            auto *child = new QTreeWidgetItem{{entry.first.c_str(), entry.second.c_str()}};
+            child->setFlags(child->flags() | Qt::ItemIsEditable);
+            treeTop->addChild(child);
         }
+    }
+}
+
+void PWParam::addElement()
+{
+    auto& nl = curParam->*curNL;
+    uint8_t idx{0};
+    std::string key = "newKey";
+    if(nl.find(key) != nl.end()){
+        std::string key2 = key + "0";
+        while(nl.find(key2) != nl.end()){
+            key2 = key + std::to_string(++idx);
+        }
+        key = key2;
+    }
+    auto *child = new QTreeWidgetItem{{key.c_str(), nl[key].c_str()}};
+    child->setFlags(child->flags() | Qt::ItemIsEditable);
+    if(!curItem->parent()){
+        curItem->addChild(child);
+    }else{
+        curItem->parent()->addChild(child);
+    }
+}
+
+void PWParam::delElement()
+{
+    auto& nl = curParam->*curNL;
+    nl.erase(nl.find(curKey));
+    delete curItem;
+}
+
+void PWParam::on_paramTree_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *)
+{
+    curItem = current;
+    const std::map<QString, IO::PWNamelist IO::PWParam::*> stringToNL =
+    {
+        {"control", &IO::PWParam::control},
+        {"system", &IO::PWParam::system},
+        {"electrons", &IO::PWParam::electrons},
+        {"ions", &IO::PWParam::ions},
+        {"cell", &IO::PWParam::cell},
+    };
+    if(!curItem->parent()){
+        delAction->setDisabled(true);
+        curNL = stringToNL.at(curItem->data(0, 0).toString().toLower());
+    }else{
+        delAction->setEnabled(true);
+        curNL = stringToNL.at(curItem->parent()->data(0, 0).toString().toLower());
+        curKey = curItem->data(0, 0).toString().toStdString();
+    }
+}
+
+void PWParam::on_paramTree_itemChanged(QTreeWidgetItem *item, int column)
+{
+    auto& nl = curParam->*curNL;
+    if(column == 0){
+        auto newKey = item->data(0, 0).toString().toStdString();
+        if(nl.find(newKey) != nl.end()){
+            //TODO: alert user that he's trying to insert key twice?
+            item->setData(0, 0, QString::fromStdString(curKey));
+            return;
+        }
+        nl[newKey] = nl.at(curKey);
+        nl.erase(nl.find(curKey));
+        curKey = std::move(newKey);
+    }else{
+        nl.at(curKey) = item->data(1, 0).toString().toStdString();
     }
 }
