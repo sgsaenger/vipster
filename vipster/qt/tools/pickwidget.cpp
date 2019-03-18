bc11627c85b8b25e9cf6ee734c3e6d01863c19cc
Sebastian Gsänger <sebastian_gsaenger@web.de>
2019-03-18 17:57:09 +0100

    use real atom idx in pick-widget

--- vipster/qt/tools/pickwidget.cpp
+++ vipster/qt/tools/pickwidget.cpp
@@ -20,7 +20,7 @@ inline void printDist(QPlainTextEdit& text,
     QString tmp = "Dist " +
             QString::number(idx1) + '-' +
             QString::number(idx2) + ": " +
-            QString::number(static_cast<double>(dist)) + "Å";
+            QString::number(static_cast<double>(dist)) + " Å";
     text.appendPlainText(tmp);
 }
 
@@ -52,15 +52,15 @@ void PickWidget::updateWidget(guiChange_t change)
     if((change & (GuiChange::atoms|GuiChange::selection)) == 0u){
         return;
     }
-    const auto curSel = master->curSel->asFmt(AtomFmt::Angstrom);
+    const auto& curSel = master->curSel->asFmt(AtomFmt::Angstrom);
+    const auto nat = curSel.getNat();
     auto& text = *ui->PickText;
-    auto idx = curSel.getIndices().begin();
+    auto idx = curSel.getIndices();//.begin();
     text.setPlainText("Atoms:");
-    for(const auto& i:curSel){
-        text.appendPlainText(QString::number(*idx)+'('+QString::fromStdString(i.name)+')');
-        idx++;
+    for(size_t i=0; i<nat; ++i){
+        text.appendPlainText(QString::number(idx[i])+'('+
+                             QString::fromStdString(curSel[i].name)+')');
     }
-    auto nat = curSel.getNat();
     if(nat>4){
         //Don't display additional information when too many atoms are selected
         return;
@@ -79,14 +79,14 @@ void PickWidget::updateWidget(guiChange_t change)
     if(nat>1){
         auto diff01 = curSel[0].coord - curSel[1].coord;
         auto dist01 = Vec_length(diff01);
-        printDist(text, 0, 1, dist01);
+        printDist(text, idx[0], idx[1], dist01);
         if(nat>2){
             auto diff02 = curSel[0].coord - curSel[2].coord;
             auto diff12 = curSel[1].coord - curSel[2].coord;
             auto dist02 = Vec_length(diff02);
             auto dist12 = Vec_length(diff12);
-            printDist(text, 0, 2, dist02);
-            printDist(text, 1, 2, dist12);
+            printDist(text, idx[0], idx[2], dist02);
+            printDist(text, idx[1], idx[2], dist12);
             if(nat == 3){
                 auto ang012 = (std::acos(Vec_dot(diff01, diff12) / (dist01 * dist12)))
                               * rad2deg;
@@ -94,9 +94,9 @@ void PickWidget::updateWidget(guiChange_t change)
                               * rad2deg;
                 auto ang201 = (std::acos(Vec_dot(diff01, diff02) / (dist01 * dist02)))
                               * rad2deg;
-                printAngle(text, 0, 1, 2, ang012);
-                printAngle(text, 1, 2, 0, ang120);
-                printAngle(text, 2, 0, 1, ang201);
+                printAngle(text, idx[0], idx[1], idx[2], ang012);
+                printAngle(text, idx[1], idx[2], idx[0], ang120);
+                printAngle(text, idx[2], idx[0], idx[1], ang201);
             }else{
                 auto diff03 = curSel[0].coord - curSel[3].coord;
                 auto diff13 = curSel[1].coord - curSel[3].coord;
@@ -104,9 +104,9 @@ void PickWidget::updateWidget(guiChange_t change)
                 auto dist03 = Vec_length(diff03);
                 auto dist13 = Vec_length(diff13);
                 auto dist23 = Vec_length(diff23);
-                printDist(text, 0, 3, dist03);
-                printDist(text, 1, 3, dist13);
-                printDist(text, 2, 3, dist23);
+                printDist(text, idx[0], idx[3], dist03);
+                printDist(text, idx[1], idx[3], dist13);
+                printDist(text, idx[2], idx[3], dist23);
                 auto cross012 = Vec_cross(diff01, diff12);
                 auto cross013 = Vec_cross(diff01, diff13);
                 auto cross023 = Vec_cross(diff02, diff23);
@@ -127,12 +127,12 @@ void PickWidget::updateWidget(guiChange_t change)
                                  * rad2deg;
                 auto dihed2013 = (std::acos(Vec_dot(cross012, cross013) / (len012 * len013)))
                                  * rad2deg;
-                printDihed(text, 0, 1, 2, 3, dihed0123);
-                printDihed(text, 0, 1, 3, 2, dihed0132);
-                printDihed(text, 1, 0, 2, 3, dihed1023);
-                printDihed(text, 1, 2, 3, 0, dihed1230);
-                printDihed(text, 1, 0, 3, 2, dihed1032);
-                printDihed(text, 2, 0, 1, 3, dihed2013);
+                printDihed(text, idx[0], idx[1], idx[2], idx[3], dihed0123);
+                printDihed(text, idx[0], idx[1], idx[3], idx[2], dihed0132);
+                printDihed(text, idx[1], idx[0], idx[2], idx[3], dihed1023);
+                printDihed(text, idx[1], idx[2], idx[3], idx[0], dihed1230);
+                printDihed(text, idx[1], idx[0], idx[3], idx[2], dihed1032);
+                printDihed(text, idx[2], idx[0], idx[1], idx[3], dihed2013);
             }
         }
     }
