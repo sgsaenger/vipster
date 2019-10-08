63b9da145f94174c4ce145a5e0471e47be3f6d2f
Sebastian Gsänger <sebastian_gsaenger@web.de>
2019-10-08 12:00:44 +0200

    revert non-working changes to make-appimage

--- util/make-appimage.sh
+++ util/make-appimage.sh
@@ -8,9 +8,7 @@ cmake -DDESKTOP=ON -DPYTHON=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX
 make DESTDIR=AppDir install -j2
 # copy python standard-library and add libpython to LD_LIBRARY_PATH
 export PY_LIB_DIR=$(python -c "from distutils import sysconfig as s; print(s.get_python_lib(standard_lib=True))")
-    cd $PY_LIB_DIR
-zip -rq $TRAVIS_BUILD_DIR/release/AppDir/$PY_LIB_DIR.zip .
-cd -
+cp -r $PY_LIB_DIR/. AppDir/$PY_LIB_DIR
 export LD_LIBRARY_PATH=$(python -c "from distutils import sysconfig as s; print(s.get_config_var('LIBDIR'))")
 # fill AppDir with dependencies
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage" -O linuxdeployqt
@@ -18,7 +16,7 @@ chmod +x linuxdeployqt
 ./linuxdeployqt AppDir/usr/share/applications/vipster.desktop -bundle-non-qt-libs;
 # move libpython so we can use possibly use system's version
 mkdir -p AppDir/usr/optional/python
-mv /usr/libpython* AppDir/usr/optional/python
+mv AppDir/usr/libpython* AppDir/usr/optional/python
 # bundle libstdc++ to be compatible with older linuxes, see https://github.com/darealshinji/AppImageKit-checkrt
 mkdir -p AppDir/usr/optional/libstdc++
 wget -c https://github.com/darealshinji/AppImageKit-checkrt/releases/download/continuous/exec-x86_64.so -O AppDir/usr/optional/exec.so
