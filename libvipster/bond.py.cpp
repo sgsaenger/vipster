79c6117bf9058b860170041965410834cece4048
Sebastian Gsänger <sebastian_gsaenger@web.de>
2019-12-06 18:37:25 +0100

    improve python bindings for bonds

--- libvipster/bond.py.cpp
+++ libvipster/bond.py.cpp
@@ -1,5 +1,21 @@
 #include "pyvipster.h"
 #include "bond.h"
+#include <pybind11/stl.h>
+
+namespace Vipster{
+std::ostream& operator<<(std::ostream& os, const Vipster::Bond&b){
+    os << "Bond(" << b.at1 << ", " << b.at2 << ", <"
+       << b.diff[0] << ", " << b.diff[1] << ", " << b.diff[2]
+       << '>';
+    if(b.type){
+        os << ", " << b.type->first;
+    }
+    os << ')';
+    return os;
+}
+}
+
+PYBIND11_MAKE_OPAQUE(std::vector<Vipster::Bond>);
 
 namespace Vipster::Py{
 void Bond(py::module &m){
@@ -10,7 +26,13 @@ void Bond(py::module &m){
         .def_readwrite("at2", &Bond::at2)
         .def_readwrite("dist", &Bond::dist)
         .def_readwrite("diff", &Bond::diff)
-        .def_property_readonly("type", [](const Vipster::Bond &b){return b.type->first;})
+        .def_property_readonly("type", [](const Vipster::Bond &b)->std::string{
+            if(b.type){
+                return b.type->first;
+            }else{
+                return "";
+            }
+        })
     ;
 
     py::enum_<BondMode>(m, "Mode")
