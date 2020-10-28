8cf1cdfd0752171f51c12d2c88cc87112d243b8c
Sebastian Gsänger <sebastian_gsaenger@web.de>
2020-10-28 16:29:17 +0100

    CI: windows: don't use obsolete --release flag (QTBUG-84567)

--- util/make-winarchive.sh
+++ util/make-winarchive.sh
@@ -7,6 +7,6 @@ cp vipster.exe Vipster
 cp libvipster.dll Vipster
 cp $pythonLocation/python39.dll Vipster
 7z a -tzip Vipster/python39.zip $pythonLocation/Lib/*
-windeployqt --release --compiler-runtime --no-translations Vipster/vipster.exe
-cp $QTDIR/bin/lib{gcc_s_seh-1,stdc++-6,winpthread-1}.dll Vipster
+windeployqt --compiler-runtime --no-translations Vipster/vipster.exe
+cp $Qt5_Dir/bin/lib{gcc_s_seh-1,stdc++-6,winpthread-1}.dll Vipster
 7z a Vipster-Win-x86_64.7z Vipster
