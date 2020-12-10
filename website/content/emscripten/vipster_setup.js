2efa54a3058e8bcdf49f09803ed9bba83f5a4878
Sebastian Gsänger <sebastian_gsaenger@web.de>
2020-12-10 20:01:52 +0100

    WebVersion: fix usage on page, reenable wasm
    - reenable wasm
    - convert page to module
    - expose FS-library in module

--- website/content/emscripten/vipster_setup.js
+++ website/content/emscripten/vipster_setup.js
@@ -1,13 +1,22 @@
 Module().then(function (Module) {
-    const VERBOSE = true;
+    const VERBOSE = false;
     const DESKTOP_BREAKPOINT = 992;
 
     const dom = {};
     [
-        'alerts', 'atList', 'btnDownload', 'btnUpload', 'canvas',
-        'cellDim', 'cellToMol', 'cellVec', 'checkboxCellEnabled',
-        'fileType', 'fileDrop', 'fileName', 'inputFile', 'loadFileModal', 'moleculeDropdown',
-        'noWebGL2Modal', 'saveFileType', 'selectAtomFormat', 'stepCur', 'stepMax', 'stepSlider', 'uploadGroup'
+        // general
+        'alerts', 'canvas', 'noWebGL2Modal',
+        // file handling
+        'fileType', 'fileDrop', 'fileName', 'inputFile',
+        'btnLoad', 'btnSave', 'btnDownload', 'btnUpload',
+        'loadFileModal', 'saveFileModal', 'saveFileType', 'uploadGroup',
+        // mol/step handling
+        'moleculeDropdown', 'stepCur', 'stepMax', 'stepSlider',
+        // atom handling
+        'atomList', 'atomFmt',
+        // cell handling
+        'cellDim', 'cellToMol', 'cellVec', 'cellEnabled', 'cellContainer',
+        'xmult', 'ymult', 'zmult'
     ].forEach(id => {
         dom[id] = document.getElementById(id);
     });
@@ -15,11 +24,12 @@ Module().then(function (Module) {
     Module.curMol = null;
     Module.curStep = null;
     Module.canvas = setupCanvas(dom.canvas);
-    Module.hammer = null;
+    Module.hammer = setupHammer(dom.canvas);
     Module.file = null;
     Module.gui = null;
     Module.molecules = [];
-    
+
+    // msg for update routine
     const change = {
         atoms: 1,
         cell: 2,
@@ -28,7 +38,6 @@ Module().then(function (Module) {
         param: 16,
         config: 32
     };
-
     change.step = change.atoms | change.cell;
     change.mol = change.kpoints;
 
@@ -38,9 +47,9 @@ Module().then(function (Module) {
     }catch(err){
         dom.noWebGL2Modal.style.display = "block";
     }
-    Module.molecules.push(new Module.Molecule('[{"atoms":[{"coord":[-0.756,-0.591,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.756,-0.591,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.736,-0.571,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.736,-0.571,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.716,-0.5509999999999999,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.716,-0.5509999999999999,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.696,-0.5309999999999999,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.696,-0.5309999999999999,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.716,-0.5509999999999999,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.716,-0.5509999999999999,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.736,-0.571,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.736,-0.571,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.756,-0.591,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.756,-0.591,0.0],"name":"H"}],"fmt":"angstrom"}]'));
+    Module.molecules.push(new Module.Molecule('{"molecule":[{"atoms":[{"coord":[-0.756,-0.591,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.756,-0.591,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.736,-0.571,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.736,-0.571,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.716,-0.5509999999999999,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.716,-0.5509999999999999,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.696,-0.5309999999999999,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.696,-0.5309999999999999,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.716,-0.5509999999999999,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.716,-0.5509999999999999,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.736,-0.571,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.736,-0.571,0.0],"name":"H"}],"fmt":"angstrom"},{"atoms":[{"coord":[-0.756,-0.591,0.0],"name":"H"},{"coord":[0.0,0.0,0.0],"name":"O"},{"coord":[0.756,-0.591,0.0],"name":"H"}],"fmt":"angstrom"}]}'));
     Module.molecules[0].name = "Example Molecule"
-    Module.molecules.push(new Module.Molecule('[{"atoms":[{"coord":[0.0,0.0,0.0],"name":"Na"},{"coord":[0.5,0.0,0.0],"name":"Cl"},{"coord":[0.5,0.5,0.0],"name":"Na"},{"coord":[0.0,0.5,0.0],"name":"Cl"},{"coord":[0.5,0.0,0.5],"name":"Na"},{"coord":[0.0,0.0,0.5],"name":"Cl"},{"coord":[0.0,0.5,0.5],"name":"Na"},{"coord":[0.5,0.5,0.5],"name":"Cl"}],"cell":{"dimension":5.64,"vectors":[[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]]},"fmt":"crystal"}]'));
+    Module.molecules.push(new Module.Molecule('{"molecule":{"atoms":[{"coord":[0.0,0.0,0.0],"name":"Na"},{"coord":[0.5,0.0,0.0],"name":"Cl"},{"coord":[0.5,0.5,0.0],"name":"Na"},{"coord":[0.0,0.5,0.0],"name":"Cl"},{"coord":[0.5,0.0,0.5],"name":"Na"},{"coord":[0.0,0.0,0.5],"name":"Cl"},{"coord":[0.0,0.5,0.5],"name":"Na"},{"coord":[0.5,0.5,0.5],"name":"Cl"}],"cell":{"dimension":5.64,"vectors":[[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]]},"fmt":"crystal"}}'));
     Module.molecules[1].name = "Example Crystal"
     setMol(0);
     for(i = 0; i < Module.nplug(); i++){
@@ -76,56 +85,59 @@ Module().then(function (Module) {
         hammer.remove(['tap', 'doubletap', 'swipe']);
 
         hammer.on('press', (e) => {
+            if (VERBOSE) { console.log('press'); }
+
             if (e.pointerType !== 'touch'){
                 return;
             }
-
-            console.log('press');
         });
         hammer.on('pressup', (e) => {
+            if (VERBOSE) { console.log('pressup'); }
+
             if (e.pointerType !== 'touch'){
                 return;
             }
-
-            console.log('pressup');
         });
 
         hammer.on('panstart', (e) => {
+            if (VERBOSE) { console.log('panstart'); }
+
             if (e.pointerType !== 'touch') {
                 return;
             }
 
-            console.log('panstart');
             this.oldX = e.deltaX;
             this.oldY = e.deltaY;
         });
 
         hammer.on('pan', (e) => {
+            if (VERBOSE) { console.log('pan'); }
+
             if (e.pointerType !== 'touch') {
                 return;
             }
 
-            console.log('pan');
             Module.gui.rotate(e.deltaX - this.oldX, e.deltaY - this.oldY);
             this.oldX = e.deltaX;
             this.oldY = e.deltaY;
         });
 
         hammer.on('pinchstart', (e) => {
+            if (VERBOSE) { console.log('pinchstart'); }
+
             if (e.pointerType !== 'touch') {
                 return;
             }
 
-            console.log('pinchstart');
             this.oldScale = 0.0
         });
         hammer.on('pinch', (e) => {
+            if (VERBOSE) { console.log('pinch'); console.log(e.scale); }
+
             if (e.pointerType !== 'touch') {
                 return;
             }
 
-            console.log('pinch');
-                      console.log(e.scale);
             const delta = (e.scale - this.oldScale) < 0 ? 0.98 : 1.02;
             Module.gui.zoom(delta);
             this.oldScale = e.scale;
@@ -168,7 +180,7 @@ Module().then(function (Module) {
         }
         html += '</tbody>';
 
-        dom.atList.innerHTML = html;
+        dom.atomList.innerHTML = html;
     }
 
     function fillCell() {
@@ -184,8 +196,9 @@ Module().then(function (Module) {
         }
     }
 
-    function atomChanged(tgt) {
-        const fmt = parseInt(dom.selectAtomFormat.value);
+    dom.atomList.oninput = function (ev) {
+        tgt = ev.target;
+
         const at = Module.curStep.getAtom( parseInt(tgt.parentElement.dataset.idx));
 
         if (tgt.dataset.idx === 'name') {
@@ -203,14 +216,16 @@ Module().then(function (Module) {
         update(change.atoms);
     }
 
-    function cellDimChanged(tgt) {
-        const newVal = parseFloat(tgt.value);
+    dom.atomFmt.onchange = function() {
+        update(change.fmt);
+    }
+
+    dom.cellDim.onchange = function () {
+        const newVal = parseFloat(dom.cellDim.value);
         if (Number.isNaN(newVal)) {
             return;
         }
 
-        //const fmt = parseInt(dom.cdmFmtSel.value);
-        //const scale = dom.cellScale.checked;
         const trajec = dom.cellToMol.checked;
 
         if (trajec) {
@@ -224,20 +239,28 @@ Module().then(function (Module) {
         update(change.cell);
     }
 
-    function cellEnabled(val) {
+    dom.cellEnabled.onchange = function () {
+        checked = dom.cellEnabled.checked;
         const trajec = dom.cellToMol.checked;
         if (trajec) {
             for (let i = 0; i < Module.curMol.nstep; ++i) {
-                Module.curMol.getStep(i).hasCell = val;
+                Module.curMol.getStep(i).hasCell = checked;
             }
         } else {
-            Module.curStep.hasCell = val;
+            Module.curStep.hasCell = checked;
         }
+
+//        if (checked) {
+//            dom.cellContainer.style = "display: block";
+//        } else {
+//            dom.cellContainer.style = "display: none";
+//        }
 
         update(change.cell);
     }
 
-    function cellVecChanged(tgt) {
+    dom.cellVec.oninput = function (ev) {
+        tgt = ev.target;
         const newVal = parseFloat(tgt.innerText);
         if (isNaN(newVal)) {
             return;
@@ -262,7 +285,7 @@ Module().then(function (Module) {
         update(change.cell);
     }
 
-    function loadDialog() {
+    dom.btnLoad.onclick = function() {
         dom.inputFile.value = "";
         dom.fileDrop.style.display = "";
         dom.uploadGroup.style.display = "none";
@@ -277,14 +300,22 @@ Module().then(function (Module) {
         Module.file = file;
     }
 
-    function dropHandler(ev){
+    dom.inputFile.onchange = function () {
+        selectFile(dom.inputFile.files[0]);
+    }
+
+    dom.fileDrop.onclick = function () {
+        dom.inputFile.click();
+    }
+
+    dom.fileDrop.ondrop = function (ev) {
         ev.preventDefault();
         if(ev.dataTransfer.files.length === 1){
             selectFile(ev.dataTransfer.files[0]);
         }
     }
 
-    function readFile() {
+    dom.btnUpload.onclick = function () {
         $('.alert').alert('close');
 
         //const file = dom.inputFile.files[0];
@@ -295,7 +326,7 @@ Module().then(function (Module) {
         const reader = new FileReader();
 
         reader.onload = (e) => {
-            FS.createDataFile('/tmp', Module.file.name, e.target.result, true);
+            Module.FS.createDataFile('/tmp', Module.file.name, e.target.result, true);
             try{
                 Module.molecules.push(new Module.Molecule('/tmp/'+Module.file.name, parseInt(dom.fileType.value)));
             }catch(err){
@@ -305,7 +336,7 @@ Module().then(function (Module) {
                 }
                 return false;
             }
-            FS.unlink('/tmp/'+Module.file.name);
+            Module.FS.unlink('/tmp/'+Module.file.name);
 
             // noinspection JSCheckFunctionSignatures
             const idx = Module.molecules.length - 1;
@@ -324,11 +355,11 @@ Module().then(function (Module) {
         $('#loadFileModal').modal('hide');
     }
 
-    function saveDialog() {
+    dom.btnSave.onclick = function () {
         $('#saveFileModal').modal();
     }
 
-    function saveFile() {
+    dom.btnDownload.onclick = function () {
         const step = parseInt(dom.stepSlider.value);
         const writeError = Module.writeFile(Module.curMol, step, parseInt(dom.saveFileType.value));
         if (writeError.length) {
@@ -338,27 +369,22 @@ Module().then(function (Module) {
             }
             return false;
         }
-        var data = FS.readFile('/tmp/output.file');
-        FS.unlink('/tmp/output.file');
+        var data = Module.FS.readFile('/tmp/output.file');
+        Module.FS.unlink('/tmp/output.file');
         var blob = new Blob([data.buffer], {type: "text/plain"});
         var url = window.URL.createObjectURL(blob);
         this.href = url;
         this.target = '_blank';
         this.download = Module.addExtension(Module.curMol.name, parseInt(dom.saveFileType.value));
-        console.log(this);
     }
 
-    function setMult() {
-        const x = document.getElementById('xmult').value;
-        const y = document.getElementById('ymult').value;
-        const z = document.getElementById('zmult').value;
-        Module.gui.mult = [parseInt(x), parseInt(y), parseInt(z)];
-    }
+    dom.xmult.onchange = dom.ymult.onchange = dom.zmult.onchange = function(){
+        Module.gui.mult = [parseInt(dom.xmult.value), parseInt(dom.ymult.value), parseInt(dom.zmult.value)];
+    };
 
     function update(arg) {
         if (arg & change.fmt) {
-            Module.curStep.fmt = parseInt(dom.selectAtomFormat.value) -2;
-            console.log(Module.curStep.fmt);
+            Module.curStep.fmt = parseInt(dom.atomFmt.value) -2;
             if (Module.curStep.fmt < 0) {
                 arg = change.cell;
             }
@@ -386,10 +412,14 @@ Module().then(function (Module) {
         $('.if-cell').toggle(hasCell);
 
         dom.stepCur.innerHTML = i + 1;
-        dom.selectAtomFormat.value = Module.curStep.fmt + 2;
-        dom.checkboxCellEnabled.checked = hasCell;
+        dom.atomFmt.value = Module.curStep.fmt + 2;
+        dom.cellEnabled.checked = hasCell;
 
         update(change.step);
+    }
+
+    dom.stepSlider.oninput = function () {
+        setStep(parseInt(dom.stepSlider.value));
     }
 
     function setMol(idx) {
@@ -429,24 +459,13 @@ Module().then(function (Module) {
             $('main').show();
         }
     }
+    window.resize = resizeCanvas;
 
     $(document).ready(function () {
-        Module.hammer = setupHammer(canvas);
-
-        // Set correct canvas size on resize
-        window.addEventListener('resize', resizeCanvas);
-
-        dom.btnDownload.onclick = saveFile;
-
         // Molecule selecting
         $(document.body).on('click', `#${dom.moleculeDropdown.id} a`, function () {
             const idx = $(this).data('idx') || 0;
             setMol(idx);
-        });
-
-        // Hide specific cell properties if no cell present
-        $(dom.checkboxCellEnabled).change(function () {
-            $('.if-cell').toggle($(this).get(0).checked);
         });
 
         $('.widget_toggle').click(function () {
