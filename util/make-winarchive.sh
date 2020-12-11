2272ab68fe322c8fb522236c707788202342c9fe
Sebastian Gsänger <sebastian_gsaenger@web.de>
2020-12-11 19:37:09 +0100

    CI: package windows release in regular zip archive

--- util/make-winarchive.sh
+++ util/make-winarchive.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-# create a 7z file from a pre-built tree
+# create a zip file from a pre-built tree
 
-echo "creating 7z-Archive"
+echo "creating zip-Archive"
 mkdir Vipster
 cp vipster.exe Vipster
 cp libvipster.dll Vipster
@@ -9,4 +9,4 @@ cp $pythonLocation/python38.dll Vipster
 cp -r $pythonLocation/Lib $pythonLocation/DLLs Vipster
 windeployqt --compiler-runtime --no-translations Vipster/vipster.exe
 cp $Qt5_Dir/bin/lib{gcc_s_seh-1,stdc++-6,winpthread-1}.dll Vipster
-7z a Vipster-Win-x86_64.7z Vipster
+7z a Vipster-Win-x86_64.zip Vipster
