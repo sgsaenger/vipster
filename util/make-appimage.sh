fcc09c9a1e2786ec8814de40eb33546a84a9102e
Sebastian Gsänger <sebastian_gsaenger@web.de>
2022-08-29 18:21:19 +0200

    Improve packaging scripts

--- util/make-appimage.sh
+++ util/make-appimage.sh
@@ -10,6 +10,18 @@ SOURCE_DIR=$(grep Vipster_SOURCE_DIR CMakeCache.txt | cut -d "=" -f 2)
 PY_BIN=$(grep "Python3_EXECUTABLE" CMakeCache.txt | cut -d "=" -f 2)
 PY_PREFIX=$(${PY_BIN} -c "import sys; print(sys.prefix)")
 
+# install hook for dynamic selection of python runtime
+mkdir -p AppDir/apprun-hooks
+sed "s|X_PYROOT_X|${PY_PREFIX}|" "${SOURCE_DIR}/util/select_python_hook.sh" | sed "s|X_PYBIN_X|${PY_BIN##*/}|" > AppDir/apprun-hooks/select_python_hook.sh
+
+# use linuxdeploy to manage dependencies
+wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt-x86_64.sh
+wget -q https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-x86_64.AppImage
+chmod +x linuxdeploy*
+./linuxdeploy-x86_64.AppImage --appdir AppDir -p qt -p checkrt
+
 # bundle the python installation used for building
 PY_LIB_DIR=$(${PY_BIN} -c "import sysconfig as s; print(s.get_path('stdlib'))")
 mkdir -p AppDir/$PY_LIB_DIR
@@ -19,13 +31,5 @@ cp -r $PY_LIB_DIR/. AppDir/$PY_LIB_DIR
 mkdir -p AppDir/usr/optional/python
 mv AppDir/usr/lib/libpython* AppDir/usr/optional/python
 
-# install hook for dynamic selection of python runtime
-mkdir -p AppDir/apprun-hooks
-sed "s|X_PYROOT_X|${PY_PREFIX}|" "${SOURCE_DIR}/util/select_python_hook.sh" | sed "s|X_PYBIN_X|${PY_BIN##*/}|" > AppDir/util/select_python_hook.sh
-
-# bundle AppDir as AppImage
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt-x86_64.sh
-chmod +x linuxdeploy*
-OUTPUT=Vipster-Linux-x86_64.AppImage ./linuxdeploy-x86_64.AppImage --appdir AppDir -o appimage -p qt -p checkrt
+# finish by creating AppImage
+ARCH=x86_64 OUTPUT=Vipster-Linux-x86_64.AppImage ./linuxdeploy-plugin-appimage-x86_64.AppImage --appdir AppDir
--- util/make-win-archive.sh
+++ util/make-win-archive.sh
@@ -3,10 +3,18 @@
 
 echo "creating zip-Archive"
 mkdir Vipster
+
+# Vipster itself
 cp vipster.exe Vipster
 cp libvipster.dll Vipster
-cp $pythonLocation/python38.dll Vipster
+
+# copy the local python installation
+PY_LIB=$(grep "Python3_LIBRARY_RELEASE" CMakeCache.txt | cut -d "=" -f 2)
+cp $PY_LIB Vipster
 cp -r $pythonLocation/Lib $pythonLocation/DLLs Vipster
+
+# prepare Qt and other libraries
 windeployqt --compiler-runtime --no-translations Vipster/vipster.exe
 cp $Qt5_Dir/bin/lib{gcc_s_seh-1,stdc++-6,winpthread-1}.dll Vipster
+
 7z a Vipster-Win-x86_64.zip Vipster
