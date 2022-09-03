a536fb65a3d2978df2804dfb62b25b4a5c2903b3
Sebastian Gsänger <sebastian_gsaenger@web.de>
2022-09-03 21:11:07 +0200

    formatting

--- vipster/vec.h
+++ vipster/vec.h
@@ -22,151 +22,153 @@ inline bool float_comp(T a, T b)
 
 inline bool operator==(const Vec &v1, const Vec &v2)
 {
-    return float_comp(v1[0], v2[0]) && float_comp(v1[1], v2[1]) && float_comp(v1[2], v2[2]);
+    return float_comp(v1[0], v2[0]) &&
+           float_comp(v1[1], v2[1]) &&
+           float_comp(v1[2], v2[2]);
 }
 
 inline bool operator!=(const Vec &v1, const Vec &v2)
 {
-    return !(v1==v2);
+    return !(v1 == v2);
 }
 
 inline Vec& operator +=(Vec &v1, const Vec &v2)
 {
-    v1[0]+=v2[0];
-    v1[1]+=v2[1];
-    v1[2]+=v2[2];
+    v1[0] += v2[0];
+    v1[1] += v2[1];
+    v1[2] += v2[2];
     return v1;
 }
 
 inline Vec operator +(Vec v1, const Vec &v2)
 {
-    return v1+=v2;
+    return v1 += v2;
 }
 
 inline Vec& operator +=(Vec &v, const double &f)
 {
-    v[0]+=f;
-    v[1]+=f;
-    v[2]+=f;
+    v[0] += f;
+    v[1] += f;
+    v[2] += f;
     return v;
 }
 
 inline Vec operator +(Vec v, const double &f)
 {
-    return v+=f;
+    return v += f;
 }
 
 inline Vec operator -(const Vec &v)
 {
-    return {{-v[0],-v[1],-v[2]}};
+    return {{-v[0], -v[1], -v[2]}};
 }
 
 inline Vec& operator -=(Vec &v1, const Vec &v2)
 {
-    v1[0]-=v2[0];
-    v1[1]-=v2[1];
-    v1[2]-=v2[2];
+    v1[0] -= v2[0];
+    v1[1] -= v2[1];
+    v1[2] -= v2[2];
     return v1;
 }
 
 inline Vec operator -(Vec v1, const Vec &v2)
 {
-    return v1-=v2;
+    return v1 -= v2;
 }
 
 inline Vec& operator -=(Vec &v, const double &f)
 {
-    v[0]-=f;
-    v[1]-=f;
-    v[2]-=f;
+    v[0] -= f;
+    v[1] -= f;
+    v[2] -= f;
     return v;
 }
 
 inline Vec operator -(Vec v, const double &f)
 {
-    return v-=f;
+    return v -= f;
 }
 
 inline Vec& operator *=(Vec &v, const double &f)
 {
-    v[0]*=f;
-    v[1]*=f;
-    v[2]*=f;
+    v[0] *= f;
+    v[1] *= f;
+    v[2] *= f;
     return v;
 }
 
 inline Vec operator *(Vec v, const double &f)
 {
-    return v*=f;
+    return v *= f;
 }
 
 inline Vec operator *(const double &f, Vec v)
 {
-    return v*=f;
+    return v *= f;
 }
 
 inline Vec& operator /=(Vec &v, const double &f)
 {
-    v[0]/=f;
-    v[1]/=f;
-    v[2]/=f;
+    v[0] /= f;
+    v[1] /= f;
+    v[2] /= f;
     return v;
 }
 
 inline Vec operator /(Vec v, const double &f)
 {
-    return v/=f;
+    return v /= f;
 }
 
 inline double Vec_dot(const Vec &v1, const Vec &v2)
 {
-    return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
+    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
 inline double Vec_length(const Vec &v)
 {
-    return std::sqrt(Vec_dot(v,v));
+    return std::sqrt(Vec_dot(v, v));
 }
 
 inline Vec Vec_cross(const Vec &v1, const Vec &v2)
 {
-    return {{v1[1]*v2[2]-v1[2]*v2[1],
-             v1[2]*v2[0]-v1[0]*v2[2],
-             v1[0]*v2[1]-v1[1]*v2[0]}};
+    return {{v1[1] * v2[2] - v1[2] * v2[1],
+             v1[2] * v2[0] - v1[0] * v2[2],
+             v1[0] * v2[1] - v1[1] * v2[0]}};
 }
 
 inline Vec operator*(const Mat &m, const Vec &v)
 {
     Vec t;
-    t[0] = Vec_dot(m[0],v);
-    t[1] = Vec_dot(m[1],v);
-    t[2] = Vec_dot(m[2],v);
+    t[0] = Vec_dot(m[0], v);
+    t[1] = Vec_dot(m[1], v);
+    t[2] = Vec_dot(m[2], v);
     return t;
 }
 
 inline Mat& operator*=(Mat &m, const double &f)
 {
-    m[0]*=f;
-    m[1]*=f;
-    m[2]*=f;
+    m[0] *= f;
+    m[1] *= f;
+    m[2] *= f;
     return m;
 }
 
 inline Mat operator*(Mat m, const double &f)
 {
-    return m*=f;
+    return m *= f;
 }
 
 inline Mat operator*(const double &f, Mat m)
 {
-    return m*=f;
+    return m *= f;
 }
 
 inline Mat& operator/=(Mat &m, const double&f)
 {
-    m[0]/=f;
-    m[1]/=f;
-    m[2]/=f;
+    m[0] /= f;
+    m[1] /= f;
+    m[2] /= f;
     return m;
 }
 
@@ -177,9 +179,9 @@ inline Mat operator/(Mat m, const double &f)
 
 inline Mat Mat_trans(const Mat &m)
 {
-    return {{Vec{{m[0][0],m[1][0],m[2][0]}},
-             Vec{{m[0][1],m[1][1],m[2][1]}},
-             Vec{{m[0][2],m[1][2],m[2][2]}}}};
+    return {{{{m[0][0], m[1][0], m[2][0]}},
+             {{m[0][1], m[1][1], m[2][1]}},
+             {{m[0][2], m[1][2], m[2][2]}}}};
 }
 
 inline Vec operator*(const Vec &v, const Mat&m)
@@ -196,15 +198,15 @@ inline Mat& operator*=(Mat &lhs, const Mat &rhs)
 {
     Mat tmp = lhs;
     Mat mt = Mat_trans(rhs);
-    lhs[0][0] = Vec_dot(tmp[0],mt[0]);
-    lhs[0][1] = Vec_dot(tmp[0],mt[1]);
-    lhs[0][2] = Vec_dot(tmp[0],mt[2]);
-    lhs[1][0] = Vec_dot(tmp[1],mt[0]);
-    lhs[1][1] = Vec_dot(tmp[1],mt[1]);
-    lhs[1][2] = Vec_dot(tmp[1],mt[2]);
-    lhs[2][0] = Vec_dot(tmp[2],mt[0]);
-    lhs[2][1] = Vec_dot(tmp[2],mt[1]);
-    lhs[2][2] = Vec_dot(tmp[2],mt[2]);
+    lhs[0][0] = Vec_dot(tmp[0], mt[0]);
+    lhs[0][1] = Vec_dot(tmp[0], mt[1]);
+    lhs[0][2] = Vec_dot(tmp[0], mt[2]);
+    lhs[1][0] = Vec_dot(tmp[1], mt[0]);
+    lhs[1][1] = Vec_dot(tmp[1], mt[1]);
+    lhs[1][2] = Vec_dot(tmp[1], mt[2]);
+    lhs[2][0] = Vec_dot(tmp[2], mt[0]);
+    lhs[2][1] = Vec_dot(tmp[2], mt[1]);
+    lhs[2][2] = Vec_dot(tmp[2], mt[2]);
     return lhs;
 }
 
@@ -215,9 +217,9 @@ inline Mat operator*(Mat lhs, const Mat &rhs)
 
 inline double Mat_det(const Mat &m)
 {
-    return  m[0][0]*(m[1][1]*m[2][2]-m[1][2]*m[2][1])
-           +m[0][1]*(m[1][2]*m[2][0]-m[1][0]*m[2][2])
-           +m[0][2]*(m[1][0]*m[2][1]-m[1][1]*m[2][0]);
+    return m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1])
+         + m[0][1] * (m[1][2] * m[2][0] - m[1][0] * m[2][2])
+         + m[0][2] * (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
 }
 
 inline Mat Mat_inv(const Mat &m)
@@ -229,15 +231,15 @@ inline Mat Mat_inv(const Mat &m)
     }
     d = 1/d;
     Mat inv;
-    inv[0][0] = (m[1][1]*m[2][2]-m[2][1]*m[1][2])*d;
-    inv[1][0] =-(m[1][0]*m[2][2]-m[2][0]*m[1][2])*d;
-    inv[2][0] = (m[1][0]*m[2][1]-m[2][0]*m[1][1])*d;
-    inv[0][1] =-(m[0][1]*m[2][2]-m[2][1]*m[0][2])*d;
-    inv[1][1] = (m[0][0]*m[2][2]-m[2][0]*m[0][2])*d;
-    inv[2][1] =-(m[0][0]*m[2][1]-m[2][0]*m[0][1])*d;
-    inv[0][2] = (m[0][1]*m[1][2]-m[1][1]*m[0][2])*d;
-    inv[1][2] =-(m[0][0]*m[1][2]-m[1][0]*m[0][2])*d;
-    inv[2][2] = (m[0][0]*m[1][1]-m[1][0]*m[0][1])*d;
+    inv[0][0] =  (m[1][1] * m[2][2] - m[2][1] * m[1][2]) * d;
+    inv[1][0] = -(m[1][0] * m[2][2] - m[2][0] * m[1][2]) * d;
+    inv[2][0] =  (m[1][0] * m[2][1] - m[2][0] * m[1][1]) * d;
+    inv[0][1] = -(m[0][1] * m[2][2] - m[2][1] * m[0][2]) * d;
+    inv[1][1] =  (m[0][0] * m[2][2] - m[2][0] * m[0][2]) * d;
+    inv[2][1] = -(m[0][0] * m[2][1] - m[2][0] * m[0][1]) * d;
+    inv[0][2] =  (m[0][1] * m[1][2] - m[1][1] * m[0][2]) * d;
+    inv[1][2] = -(m[0][0] * m[1][2] - m[1][0] * m[0][2]) * d;
+    inv[2][2] =  (m[0][0] * m[1][1] - m[1][0] * m[0][1]) * d;
     return inv;
 }
 
