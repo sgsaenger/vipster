535323e44332e767c37e8cdd1ca199c83ba84937
Sebastian Gsänger <sebastian_gsaenger@web.de>
2023-12-25 15:48:25 +0100

    Update {fmt} to 10.1.1, fix issues

--- util/dependencies.cmake
+++ util/dependencies.cmake
@@ -31,9 +31,9 @@ FetchContent_Declare(
 FetchContent_Declare(
     fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt
-    GIT_TAG 10.0.0
+    GIT_TAG 10.1.1
     GIT_SHALLOW TRUE
-    FIND_PACKAGE_ARGS 10.0.0
+    FIND_PACKAGE_ARGS 10.1.1
 )
 
 # library: pybind11
