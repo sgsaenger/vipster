fbfbad9ecd870ce3f19b3eb2348e36ba837259d1
Sebastian Gsänger <sebastian_gsaenger@web.de>
2024-10-10 12:58:34 +0200

    Work around MSVC bug...

--- vipster/iotuple.h
+++ vipster/iotuple.h
@@ -9,9 +9,32 @@
 #include <optional>
 
 namespace Vipster{
-    using IOTuple = std::tuple<Molecule,
+    // Simple tuple of Types produced from parsing files
+    // Implemented as a class because MSVC is buggy...
+    // for some reason, it insists on using the deleted copy constructor in vector.push_back
+    class IOTuple: public std::tuple<Molecule,
                                std::optional<Parameter>,
-                               DataList>;
+                               DataList>
+    {
+    public:
+        using std::tuple<Molecule, std::optional<Parameter>, DataList>::tuple;
+        IOTuple(const IOTuple&) = delete;
+        IOTuple& operator=(const IOTuple&) = delete;
+        IOTuple(IOTuple&&) = default;
+        IOTuple& operator=(IOTuple&&) = default;
+    };
+}
+
+namespace std
+{
+    template<>
+    struct tuple_size<Vipster::IOTuple>: std::integral_constant<size_t, 3>{};
+    template<>
+    struct tuple_element<0, Vipster::IOTuple> { using type = Vipster::Molecule; };
+    template<>
+    struct tuple_element<1, Vipster::IOTuple> { using type = std::optional<Vipster::Parameter>; };
+    template<>
+    struct tuple_element<2, Vipster::IOTuple> { using type = Vipster::DataList; };
 }
 
 #endif // IODATA_H
