e5b25921625e11b436b3a9ec9ebc8b68700609d0
Sebastian Gsänger <sebastian_gsaenger@web.de>
2025-03-02 13:19:47 +0100

    Build catch2 as static lib

--- tests/unit_tests.cmake
+++ tests/unit_tests.cmake
@@ -1,7 +1,11 @@
 file(GLOB_RECURSE TEST_SOURCES "unit_tests/*.cpp")
 add_executable(test_lib ${TEST_SOURCES})
 
+# test-only dependency, hardcode to static lib for windows testing...
+set(IS_SHARED BUILD_SHARED_LIBS)
+set(BUILD_SHARED_LIBS OFF)
 find_or_download_package(Catch2)
+set(BUILD_SHARED_LIBS IS_SHARED)
 target_link_libraries(test_lib PRIVATE libvipster Catch2::Catch2WithMain)
 
 add_test(NAME test_lib COMMAND $<TARGET_FILE:test_lib>)
